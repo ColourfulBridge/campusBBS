912e27be7845230628e0aeedfc7967b3dbdb4ae6
ColourfulBridge <1160688035@qq.com>
2020-01-02 23:15:24 +0800

    update2

--- pages/favorite/favorite.js
+++ pages/favorite/favorite.js
@@ -1,13 +1,30 @@
 // pages/favorite/favorite.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    inputShowed: false,
+    inputVal: ""
   },
-
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
