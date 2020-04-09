7a359d25dcb18a837b5d424a61389bc174c8ef69
ColourfulBridge <1160688035@qq.com>
2020-04-09 22:26:52 +0800

    部署点赞云函数

---
+++ functions/good/index.js
@@ -0,0 +1,82 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const DB = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const posts = DB.collection('Post')
+  const users = DB.collection("User")
+  const comments = DB.collection("Comment")
+  const _ = DB.command;
+  console.log("进入云函数中");
+  if (!event.isgood) {
+    await users.where({
+      _openid: event.my_openid
+    }).update({
+      data: {
+        good: _.push(event.post_id)
+      }
+    })
+      .then(console.log)
+      .catch(res => {
+        console.log("点赞-用户出错1");
+        wx.showToast({
+          title: '网络出错o(TヘTo)',
+          icon: 'none',
+          duration: 2000
+        })
+      })
+
+    await posts.where({
+      _id: event.post_id
+    }).update({
+      data: {
+        good_counts: _.inc(1)
+      }
+    })
+      .then(console.log)
+      .catch(res => {
+        console.log("点赞-帖子出错1");
+        wx.showToast({
+          title: '网络出错o(TヘTo)',
+          icon: 'none',
+          duration: 2000
+        })
+      })
+  } else {
+    await users.where({
+      _openid: event.my_openid
+    }).update({
+      data: {
+        good: _.pull(event.post_id) //从post数组中删除对应的post_id//
+      }
+    })
+      .then(console.log)
+      .catch(res => {
+        console.log("点赞-用户出错2");
+        wx.showToast({
+          title: '网络出错o(TヘTo)',
+          icon: 'none',
+          duration: 2000
+        })
+      })
+
+    await posts.where({
+      _id: event.post_id
+    }).update({
+      data: {
+        good_counts: _.inc(-1)
+      }
+    })
+      .then(console.log)
+      .catch(res => {
+        console.log("点赞-帖子出错2");
+        wx.showToast({
+          title: '网络出错o(TヘTo)',
+          icon: 'none',
+          duration: 2000
+        })
+      })
+  }
+}
--- pages/hot/hot.js
+++ pages/hot/hot.js
@@ -440,75 +440,90 @@ Page({
       [good_counts]: count + 1
     });
 
-    if (!isgood) {
-      users.where({
-        _openid: app.globalData.my_openid
-      }).update({
-        data: {
-          good: _.push(post_id)
-        }
-      })
-        .then(console.log)
-        .catch(res => {
-          console.log("点赞-用户出错1");
-          wx.showToast({
-            title: '网络出错o(TヘTo)',
-            icon: 'none',
-            duration: 2000
-          })
-        })
 
-      posts.where({
-        _id: post_id
-      }).update({
-        data: {
-          good_counts: _.inc(1)
-        }
-      })
-        .then(console.log)
-        .catch(res => {
-          console.log("点赞-帖子出错1");
-          wx.showToast({
-            title: '网络出错o(TヘTo)',
-            icon: 'none',
-            duration: 2000
-          })
-        })
-    } else {
-      users.where({
-        _openid: app.globalData.my_openid
-      }).update({
-        data: {
-          good: _.pull(post_id) //从post数组中删除对应的post_id
-        }
-      })
-        .then(console.log)
-        .catch(res => {
-          console.log("点赞-用户出错2");
-          wx.showToast({
-            title: '网络出错o(TヘTo)',
-            icon: 'none',
-            duration: 2000
-          })
-        })
+    wx.cloud.callFunction({
+      name: 'good', 
+      data: {
+        my_openid:app.globalData.my_openid,
+        isgood:isgood,
+        post_id:post_id,
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: console.error
+    })
+       
+      
+    // if (!isgood) {
+    //   users.where({
+    //     _openid: app.globalData.my_openid
+    //   }).update({
+    //     data: {
+    //       good: _.push(post_id)
+    //     }
+    //   })
+    //     .then(console.log)
+    //     .catch(res => {
+    //       console.log("点赞-用户出错1");
+    //       wx.showToast({
+    //         title: '网络出错o(TヘTo)',
+    //         icon: 'none',
+    //         duration: 2000
+    //       })
+    //     })
 
-      posts.where({
-        _id: post_id
-      }).update({
-        data: {
-          good_counts: _.inc(-1)
-        }
-      })
-        .then(console.log)
-        .catch(res => {
-          console.log("点赞-帖子出错2");
-          wx.showToast({
-            title: '网络出错o(TヘTo)',
-            icon: 'none',
-            duration: 2000
-          })
-        })
-    }
+    //   posts.where({
+    //     _id: post_id
+    //   }).update({
+    //     data: {
+    //       good_counts: _.inc(1)
+    //     }
+    //   })
+    //     .then(console.log)
+    //     .catch(res => {
+    //       console.log("点赞-帖子出错1");
+    //       wx.showToast({
+    //         title: '网络出错o(TヘTo)',
+    //         icon: 'none',
+    //         duration: 2000
+    //       })
+    //     })
+    // } else {
+    //   users.where({
+    //     _openid: app.globalData.my_openid
+    //   }).update({
+    //     data: {
+    //       good: _.pull(post_id) //从post数组中删除对应的post_id
+    //     }
+    //   })
+    //     .then(console.log)
+    //     .catch(res => {
+    //       console.log("点赞-用户出错2");
+    //       wx.showToast({
+    //         title: '网络出错o(TヘTo)',
+    //         icon: 'none',
+    //         duration: 2000
+    //       })
+    //     })
+
+    //   posts.where({
+    //     _id: post_id
+    //   }).update({
+    //     data: {
+    //       good_counts: _.inc(-1)
+    //     }
+    //   })
+    //     .then(console.log)
+    //     .catch(res => {
+    //       console.log("点赞-帖子出错2");
+    //       wx.showToast({
+    //         title: '网络出错o(TヘTo)',
+    //         icon: 'none',
+    //         duration: 2000
+    //       })
+    //     })
+    // }
   },
   /**
    * 为评论点赞
